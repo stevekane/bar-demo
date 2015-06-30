de360b3c5a41eaa5e6bc593c06c1af15fb0b47d0
Steve Kane <kanesteven@gmail.com>
2015-06-29 19:52:21 -0700

    more work on transactions and state handling

--- test-transpile.js
+++ test-transpile.js
@@ -8,7 +8,7 @@ import Clock from './src/Clock'
 import Engine from './src/Engine'
 import Enum from './src/Enum'
 
-const {min, max} = Math
+const {min, max, floor, random} = Math
 
 const handleRequest = (req, res) => server(req, res, finalHander(req, res))
 const httpServer = Server(handleRequest)
@@ -16,17 +16,41 @@ const io = SocketServer(httpServer)
 const engine = new Engine(new Clock, io)
 
 const MIN_BET = 500
-const STATUS = new Enum('Active', 'Standing', 'Busted')
+const STATUS = new Enum('Active', 'Standing', 'Busted', 'BlackJack')
 const SUITS = new Enum('Diamonds', 'Hearts', 'Spades', 'Clubs')
-const CARDS = new Enum('Ace', 'One', 'Two', 'Three', 'Four', 'Five',
+const CARDS = new Enum('Ace', 'Two', 'Three', 'Four', 'Five',
                        'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack',
                        'Queen', 'King')
+const CARD_VALUES = [
+  [1, 11], [2], [3], [4], [5], [6], [7], [8], [9],
+  [10], [10], [10], [10]
+]
 
 function randFrom (obj) {
   var keys = Object.keys(obj)
-  var index = Math.floor(Math.random() * keys.length)
+  var index = floor(random() * keys.length)
 
   return obj[keys[index]]
+}
+
+function lowest (ar) {
+  var val
+
+  for (var i = 0; i < ar.length; i++) {
+    val = !val || ar[i] < val ? ar[i] : val
+  }
+
+  return val
+}
+
+function highest (ar) {
+  var val
+
+  for (var i = 0; i < ar.length; i++) {
+    val = !val || ar[i] > val ? ar[i] : val
+  }
+
+  return val
 }
 
 function contains (array, obj) {
@@ -44,6 +68,18 @@ function findWhere (prop, value, array) {
 
 function remove (array, obj) {
   array.splice(array.indexOf(obj), 1)
+}
+
+//TODO: maybe make this use the magical arguments object?
+function addLists (list1, list2) {
+  var results = []
+
+  for (var i = 0; i < list1.length; i++) {
+    for (var j = 0; j < list2.length; j++) {
+      results.push(list1[i] + list2[j]) 
+    } 
+  }
+  return results
 }
 
 class Store {
@@ -96,7 +132,7 @@ class Store {
       this.remove(this.getById(entity.childIds[0])) 
     }
 
-    if (parentEntity) remove(parentEntity.childIds, entity.id)
+    if (parentEntity)                    remove(parentEntity.childIds, entity.id)
     if (contains(this.entities, entity)) remove(this.entities, entity)
     entity.parentId = null
     return this
@@ -106,7 +142,7 @@ class Store {
     let oldParent = this.getById(entity.parentId)
 
     if (!contains(this.entities, entity)) this.entities.push(entity) 
-    if (oldParent) remove(oldParent.childIds, entity.id)
+    if (oldParent)                        remove(oldParent.childIds, entity.id)
 
     entity.parentId = targetEntity.id
     targetEntity.childIds.push(entity.id)
@@ -136,10 +172,11 @@ class Card extends Entity {
 }
 
 class Hand extends Entity {
-  constructor (betValue, status=STATUS.Active) {
+  constructor (betValue, status, doubledDown=false) {
     super('Hand')
     this.betValue = betValue
     this.status = status
+    this.doubledDown = doubledDown
   }
 }
 
@@ -154,6 +191,26 @@ class Dealer extends Entity {
   constructor () {
     super('Dealer')
   }
+}
+
+class BlackJackTable extends Store {
+  constructor () {
+    super()
+    this.attach(this.root, new Dealer)
+  }
+}
+
+function calculateChipCount (bet, chipCount) {
+  return max(chipCount - bet, 0)
+}
+
+function calculateValues (cards) {
+  var values = CARD_VALUES[cards[0].name]
+
+  for (var i = 1; i < cards.length; i++) {
+    values = addLists(values, CARD_VALUES[cards[i].name])
+  }
+  return values
 }
 
 function cleanupPlayer (store, player) {
@@ -179,7 +236,7 @@ function dealPlayer (store, betValue, player) {
   let card1 = new Card.Random
   let card2 = new Card.Random
 
-  player.chipCount = max(player.chipCount - MIN_BET, 0)
+  player.chipCount = calculateChipCount(MIN_BET, player.chipCount)
   store.attach(player, newHand)
   store.attach(newHand, card1)
   store.attach(newHand, card2)
@@ -189,31 +246,30 @@ function dealRound (store) {
   let dealer = store.getChildByType(store.root, Dealer)
   let players = store.getChildrenByType(store.root, Player)
   
-  dealPlayer(store, MIN_BET, dealer)
+  dealPlayer(store, 0, dealer)
   for (var i = 0; i < players.length; i++) {
-    dealPlayer(store, 0, players[i])
+    dealPlayer(store, MIN_BET, players[i])
   }
 }
 
-function bet (store, player, hand) {
-  if (hand.betValue > 0) return
-
-  hand.currentBet = MIN_BET
-  hand.chipCount = max(player.chipCount - MIN_BET, 0)
-}
-
 function stand (store, hand) {
-  if (!(hand instanceof Hand)) return
   if (hand.status !== STATUS.Active) return
 
   hand.status = STATUS.Standing
 }
 
 function hit (store, hand) {
-  if (!(hand instanceof Hand)) return
-  if (hand.status !== STATUS.Active) return
+  if (hand.status !== STATUS.Active) return log('Not active')
+
+  let cards, values
 
   store.attach(hand, new Card.Random)
+  cards = store.getChildrenByType(hand, Card)
+  values = calculateValues(cards)  
+
+  if (lowest(values) > 21)       hand.status = STATUS.Busted
+  else if (contains(values, 21)) hand.status = STATUS.BlackJack
+  else                           hand.status = hand.status
 }
 
 function split (store, hand) {
@@ -221,32 +277,50 @@ function split (store, hand) {
   let cards = store.getChildrenByType(hand, Card)
   let newHand = new Hand(MIN_BET, STATUS.Active)
 
-  if (cards.length > 2) return console.log('More than 2 cards')
-  if (cards[0].name !== cards[1].name) return console.log('Not same value!')
-  if (hand.status !== STATUS.Active) return
-  if (!owner) return
+  if (cards.length > 2)                return log('More than 2 cards')
+  if (cards[0].name !== cards[1].name) return log('Not same value!')
+  if (hand.status !== STATUS.Active)   return log('Not active')
+  if (!owner)                          return log('no owner for hand')
   
-  owner.chipCount = max(owner.chipCount - MIN_BET, 0)
+  owner.chipCount = calculateChipCount(MIN_BET, owner.chipCount)
   store.attach(owner, newHand)
   store.attach(newHand, cards[1])
-  store.attach(hand, new Card.Random)
-  store.attach(newHand, new Card.Random)
+  hit(store, hand)
+  hit(store, newHand)
 }
 
-function doubleDown (store, player) {
+function doubleDown (store, hand) {
+  let owner = store.getById(hand.parentId)
+  let cards = store.getChildrenByType(hand, Card)
 
+  if (cards.length > 2)              return log('More than 2 cards')
+  if (!owner)                        return log('no owner for hand')
+  if (hand.status !== STATUS.Active) return log('Not active')
+  
+  owner.chipCount = calculateChipCount(MIN_BET, owner.chipCount)
+  hand.betValue *= 2
+  hand.doubledDown = true
+  hit(store, hand)
 }
 
-let store = new Store
-let dealer = new Dealer
-let player = new Player(5000)
+let table = new BlackJackTable
+let player1 = new Player(5000)
+let player2 = new Player(5000)
 
-store.attach(store.root, dealer)
-store.attach(store.root, player)
+table.attach(table.root, player1)
+table.attach(table.root, player2)
 
-dealRound(store)
-split(store, store.getChildrenByType(player, Hand)[0])
-pp(store.getByType(Card))
-cleanupRound(store)
-pp(store.getByType(Card))
-pp(store)
+dealRound(table)
+
+let targetHand = table.getChildByType(player1, Hand)
+hit(table, targetHand)
+hit(table, targetHand)
+hit(table, targetHand)
+hit(table, targetHand)
+
+pp(table.getChildrenByType(targetHand, Card))
+//split(table, table.getChildByType(player1, Hand))
+//doubleDown(table, table.getChildByType(player2, Hand))
+//pp(table)
+cleanupRound(table)
+//pp(table)
