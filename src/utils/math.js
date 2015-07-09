40fd5802b9e8e14b2c1274ae160e49d286d2af5e
Steve Kane <kanesteven@gmail.com>
2015-07-08 19:24:09 -0500

    spiked some simpler gamestate

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,5 +1,13 @@
 'use strict'
 
+export function randRange (min, max) {
+  return min + Math.floor(Math.random() * max - min)
+}
+
+export function within (min, max, val) {
+  return val >= min && val <= max
+}
+
 export function sum (a, b) {
   return a + b
 }
